d1b7df2f993372e8d839516ff69c2af2eb7ff303
Agampreet Singh Bajwa <agambajwa2207@gmail.com>
2020-07-07 22:57:33 +0530

    Initial commit

---
+++ to_do_list_app/android/app/src/main/kotlin/com/agambajwa/todolistapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.agambajwa.todolistapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ to_do_list_app/lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todolistapp/screens/home_screen.dart';
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
